8b6310f92ee1c54a25c0c3597861fcb583d998bf
Aman Kumar <amandalal899@gmail.com>
2024-09-18 19:42:53 +0530

    next-config1

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-// export default nextConfig;
-
-// next.config.mjs
-export default {
-  webpack(config, { dev }) {
-    if (!dev) {
-      // Customize your Webpack configuration
-      config.stats = {
-        warnings: true,
-        errors: true,
-      };
-    }
-    return config;
-  },
-};
+export default nextConfig;
